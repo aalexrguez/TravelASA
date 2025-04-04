143120731da479997a066752d99d97bd2e4127b3
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-04 11:03:09 -0600

    Se actualiza la vista index

--- asa_travel/views.py
+++ asa_travel/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hola prueba</h1>")
+    return render(request,'asa_travel/index.html')
