c554ffc1e060b408fe407285ff8c743968153c6e
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-03 22:46:52 -0600

    Se agrego la app a installed_apps y se configuro la base de datos mysql

--- TravelASA/settings.py
+++ TravelASA/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'asa_travel',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'TravelASA.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "travelASA",
+        "USER": "root",
+        "PASSWORD": "mysql6103021",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
