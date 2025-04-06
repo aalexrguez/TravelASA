a4d68867db6ad2c85af2aaecc48fba420b45bc50
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-05 21:37:51 -0600

    Se crean los modelos para el home

--- asa_travel/models.py
+++ asa_travel/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Destination(models.Model):
     name = models.CharField(max_length=80, unique=True)
     description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='destination',blank=True)
 
     def __str__(self):
         return self.name
