cc26190da9082f37e1f39d80851cb13262f69678
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-06 11:32:58 -0600

    Se agregaron a los modelos el campo de las imagenes

--- asa_travel/models.py
+++ asa_travel/models.py
@@ -22,6 +22,7 @@ class Attraction(models.Model):
     attraction_category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     attraction_opening_hours = models.CharField(max_length=50)
     attraction_price = models.DecimalField(max_digits=7, decimal_places=2)
+    attraction_image = models.ImageField(upload_to='attraction',blank=True)
 
     def __str__(self):
         return self.attraction_name
@@ -33,6 +34,7 @@ class FoodAndRestaurant(models.Model):
     fd_food_type = models.CharField(max_length=50)
     fd_price_range = models.CharField(max_length=10)
     fd_opening_hours = models.CharField(max_length=50)
+    fd_image = models.ImageField(upload_to='foodRestaurant',blank=True)
 
     def __str__(self):
         return self.fd_name
@@ -44,6 +46,7 @@ class Accommodation(models.Model):
     accommodation_type = models.CharField(max_length=50)
     accommodation_price_per_night = models.DecimalField(max_digits=7, decimal_places=2)
     accommodation_rating = models.FloatField()
+    accommodation_image = models.ImageField(upload_to='accommodation',blank=True)
 
     def __str__(self):
         return self.accommodation_name
@@ -55,6 +58,7 @@ class ActivityAndTour(models.Model):
     at_duration = models.CharField(max_length=50)
     at_price = models.DecimalField(max_digits=7, decimal_places=2)
     at_available_dates = models.CharField(max_length=100)
+    at_image = models.ImageField(upload_to='activity',blank=True)
 
     def __str__(self):
         return self.at_name
