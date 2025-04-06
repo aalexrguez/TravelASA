5c8bea41ee25fd433fd3ab6c1425ce4ba60e035f
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-06 02:09:03 -0600

    Se agrego el id en destination_detail

--- asa_travel/urls.py
+++ asa_travel/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+app_name = 'asa_travel'
+
 urlpatterns = [
     path('',views.index),
     path('home/',views.index),
-    path('destination_detail/',views.destination_detail)
+    path('destination_detail/<int:destination_id>',views.destination_detail,name='destination_detail')
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
