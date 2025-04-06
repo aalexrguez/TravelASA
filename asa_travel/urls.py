85a7bd5dadee3d1521f38e1260e776b3445bffd7
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-06 01:24:54 -0600

    Se agrega la ruta para el detalle del destino

--- asa_travel/urls.py
+++ asa_travel/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index),
-    path('home/',views.index)
+    path('home/',views.index),
+    path('destination_detail/',views.destination_detail)
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
