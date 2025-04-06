6e8a58b00797c1c1a74b756a7e32229e4b875fc9
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-05 22:48:33 -0600

    Se modifico la vista index para que muestre todos los destinos

--- asa_travel/views.py
+++ asa_travel/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'asa_travel/index.html')
+    destination_all = Destination.objects.all()
+    context = {
+        'destinations':destination_all
+    }
+    return render(request,'asa_travel/index.html',context)
