411322aaea2a897937a2467dd3dc9559938f5975
Alex Rodriguez <alejandro.rodriguez112m@gmail.com>
2025-04-08 22:21:11 -0600

    Se agrega el clientform

--- asa_travel/forms.py
+++ asa_travel/forms.py
@@ -5,9 +5,6 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class ClientForm(forms.Form):
-    client_first_name = forms.CharField(label='Nombre')
-    client_last_name = forms.CharField(label='Apellido')
-    client_email = forms.CharField(label='Email')
     client_sex = forms.CharField(label='Sexo')
     client_phone = forms.CharField(label='Telefono')
     client_date_of_birth = forms.DateField(label='Fecha de nacimiento',input_formats=['%Y-%m-%d'],widget=DateInput)
